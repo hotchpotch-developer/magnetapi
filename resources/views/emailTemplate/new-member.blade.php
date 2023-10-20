cb7da21ffcd83911e386a96296bec7af059a4ec8
Vishal Soni <vishal.soni@xipetech.com>
2023-10-20 15:05:57 +0530

    Update template

--- resources/views/emailTemplate/new-member.blade.php
+++ resources/views/emailTemplate/new-member.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Reset Password</title>
+    <title>New Member</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="color-scheme" content="light">
